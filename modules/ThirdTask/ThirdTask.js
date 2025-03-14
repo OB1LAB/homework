af2c603a48e5f6fa436dd3dcb865290bffa94bdb
OB1CHAM <ob1chaam@gmail.com>
2025-03-14 04:53:22 +0300

    Строение прямоугольника по 3 вершинам

--- modules/ThirdTask/ThirdTask.js
+++ modules/ThirdTask/ThirdTask.js
@@ -1,12 +1,12 @@
 "use client"
-const Decimal = require('decimal.js');
+import Decimal from "decimal.js";
 import {useEffect, useRef} from "react";
 import styles from "./ThirdTask.module.scss"
 
 const vertexes = [
   [new Decimal(500), new Decimal(400)],
   [new Decimal(500), new Decimal(350)],
-  [new Decimal(400), new Decimal(400)],
+  // [new Decimal(400), new Decimal(400)],
   [new Decimal(400), new Decimal(350)],
 ]
 
@@ -61,13 +61,13 @@ function calculateAngle(vertex0, vertex1, vertex2) {
   const magnitudeBC = BCx.pow(2).plus(BCy.pow(2)).sqrt();
   const cosTheta = dotProduct.div(magnitudeBA.mul(magnitudeBC));
   const thetaRad = Decimal.acos(cosTheta);
-  const thetaDeg = thetaRad.mul(180).div(Decimal.acos(-1)); // 180 / π
+  const thetaDeg = thetaRad.mul(new Decimal(180)).div(Decimal.acos(-1)); // 180 / π
   return thetaDeg.toNumber();
 }
 
 
 vertexes.forEach((vertex, vertexIndex) => {
-  vertexes[vertexIndex] = rotateVertex(vertex, 0)
+  vertexes[vertexIndex] = rotateVertex(vertex, 10)
 })
 
 const ThirdTask = () => {
@@ -79,9 +79,20 @@ const ThirdTask = () => {
       const sortedVertexes = getSortedVertexes(vertexes)
       const allX = [sortedVertexes[0][0], sortedVertexes[1][0], sortedVertexes[2][0]]
       const allY = [sortedVertexes[0][1], sortedVertexes[1][1], sortedVertexes[2][1]]
-      const vertex3X = allX.find((item) => allX.filter((a) => a.toNumber() === item.toNumber()).length === 1)
-      const vertex3Y = allY.find((item) => allY.filter((a) => a.toNumber() === item.toNumber()).length === 1)
+      const vertex3X = allX.find((item) => allX.filter((a) => a.eq(item)).length === 1)
+      const vertex3Y = allY.find((item) => allY.filter((a) => a.eq(item)).length === 1)
       sortedVertexes[3] = [vertex3X, vertex3Y]
+      for (let degree = 0; degree < 360; degree++) {
+        const rotatedVertex0 = rotateVertex(sortedVertexes[0], -degree);
+        const rotatedVertex1 = rotateVertex(sortedVertexes[1], -degree);
+        if (rotatedVertex0[0].eq(rotatedVertex1[0]) || rotatedVertex0[1].eq(rotatedVertex1[1])) {
+          console.log('find')
+          sortedVertexes.forEach((vertex, vertexIndex) => {
+            sortedVertexes[vertexIndex] = rotateVertex(vertex, -degree)
+          })
+          break;
+        }
+      }
       console.log(sortedVertexes[0][0].toNumber(), sortedVertexes[0][1].toNumber())
       console.log(sortedVertexes[1][0].toNumber(), sortedVertexes[1][1].toNumber())
       console.log(sortedVertexes[2][0].toNumber(), sortedVertexes[2][1].toNumber())
