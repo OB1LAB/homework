466374727b87fc8e980489ec9a8e3a076c95cd8f
OB1CHAM <ob1chaam@gmail.com>
2025-03-14 04:30:13 +0300

    Строение прямоугольника по 3 вершинам

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import FirstTask from "@/modules/FirstTask/FirstTask";
 import SecondTask from "@/modules/SecondTask/SecondTask";
+import ThirdTask from "@/modules/ThirdTask/ThirdTask";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="center">Лабораторная работа №1. Бикбулатов Виталий Юрьевич. ПРО-204Б</div>
       <FirstTask/>
       <SecondTask/>
+      <ThirdTask/>
     </div>
   );
 }
---
+++ modules/ThirdTask/ThirdTask.js
@@ -0,0 +1,100 @@
+"use client"
+const Decimal = require('decimal.js');
+import {useEffect, useRef} from "react";
+import styles from "./ThirdTask.module.scss"
+
+const vertexes = [
+  [new Decimal(500), new Decimal(400)],
+  [new Decimal(500), new Decimal(350)],
+  [new Decimal(400), new Decimal(400)],
+  [new Decimal(400), new Decimal(350)],
+]
+
+const drawLine = (ctx, x0, y0, x1, y1) => {
+  ctx.beginPath();
+  ctx.moveTo(x0, y0);
+  ctx.lineTo(x1, y1);
+  ctx.stroke();
+}
+
+const cosDegree = (degree) => {
+  const angleInDegrees = new Decimal(degree);
+  const angleInRadians = angleInDegrees.mul(Decimal.acos(-1)).div(180);
+  return Decimal.cos(angleInRadians)
+}
+
+const sinDegree = (degree) => {
+  const angleInDegrees = new Decimal(degree);
+  const radians = Decimal.div(angleInDegrees, 180).times(Math.PI);
+  return Decimal.sin(radians);
+}
+
+const rotateVertex = (vertex, degree) => {
+  return [vertex[0].mul(cosDegree(degree)).minus(vertex[1].mul(sinDegree(degree))), vertex[0].mul(sinDegree(degree)).plus(vertex[1].mul(cosDegree(degree)))]
+}
+
+const diffBetweenVertex = (vertex0, vertex1) => {
+  return [vertex0[0].minus(vertex1[0]), vertex0[0].minus(vertex1[1])]
+}
+
+const getSortedVertexes = (vertexes) => {
+  for (let index0 = 0; index0 < 3; index0++) {
+    for (let index1 = 0; index1 < 3; index1++) {
+      for (let index2 = 0; index2 < 3; index2++) {
+        if (!(index0 === index1 || index1 === index2 || index2 === index0))
+          if (calculateAngle(vertexes[index0], vertexes[index1], vertexes[index2]) === 90) {
+            return [vertexes[index0], vertexes[index1], vertexes[index2]]
+          }
+      }
+    }
+  }
+  return [];
+}
+
+function calculateAngle(vertex0, vertex1, vertex2) {
+  const BAx = vertex0[0].minus(vertex1[0]);
+  const BAy = vertex0[1].minus(vertex1[1]);
+  const BCx = vertex2[0].minus(vertex1[0]);
+  const BCy = vertex2[1].minus(vertex1[1]);
+  const dotProduct = BAx.mul(BCx).plus(BAy.mul(BCy));
+  const magnitudeBA = BAx.pow(2).plus(BAy.pow(2)).sqrt();
+  const magnitudeBC = BCx.pow(2).plus(BCy.pow(2)).sqrt();
+  const cosTheta = dotProduct.div(magnitudeBA.mul(magnitudeBC));
+  const thetaRad = Decimal.acos(cosTheta);
+  const thetaDeg = thetaRad.mul(180).div(Decimal.acos(-1)); // 180 / π
+  return thetaDeg.toNumber();
+}
+
+
+vertexes.forEach((vertex, vertexIndex) => {
+  vertexes[vertexIndex] = rotateVertex(vertex, 0)
+})
+
+const ThirdTask = () => {
+  const canvasRef = useRef()
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.lineWidth = 2;
+      const sortedVertexes = getSortedVertexes(vertexes)
+      const allX = [sortedVertexes[0][0], sortedVertexes[1][0], sortedVertexes[2][0]]
+      const allY = [sortedVertexes[0][1], sortedVertexes[1][1], sortedVertexes[2][1]]
+      const vertex3X = allX.find((item) => allX.filter((a) => a.toNumber() === item.toNumber()).length === 1)
+      const vertex3Y = allY.find((item) => allY.filter((a) => a.toNumber() === item.toNumber()).length === 1)
+      sortedVertexes[3] = [vertex3X, vertex3Y]
+      console.log(sortedVertexes[0][0].toNumber(), sortedVertexes[0][1].toNumber())
+      console.log(sortedVertexes[1][0].toNumber(), sortedVertexes[1][1].toNumber())
+      console.log(sortedVertexes[2][0].toNumber(), sortedVertexes[2][1].toNumber())
+      console.log(sortedVertexes[3][0].toNumber(), sortedVertexes[3][1].toNumber())
+      drawLine(ctx, sortedVertexes[0][0].toNumber(), sortedVertexes[0][1].toNumber(), sortedVertexes[1][0].toNumber(), sortedVertexes[1][1].toNumber())
+      drawLine(ctx, sortedVertexes[1][0].toNumber(), sortedVertexes[1][1].toNumber(), sortedVertexes[2][0].toNumber(), sortedVertexes[2][1].toNumber())
+      drawLine(ctx, sortedVertexes[2][0].toNumber(), sortedVertexes[2][1].toNumber(), sortedVertexes[3][0].toNumber(), sortedVertexes[3][1].toNumber())
+      drawLine(ctx, sortedVertexes[3][0].toNumber(), sortedVertexes[3][1].toNumber(), sortedVertexes[0][0].toNumber(), sortedVertexes[0][1].toNumber())
+    }
+  }, []);
+  return <div>
+    <canvas width={800} height={600} className={styles.canvas} ref={canvasRef}/>
+  </div>
+}
+
+export default ThirdTask
